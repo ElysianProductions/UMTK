1d3b7b47f5a05e903db8c39b560ed29f99513a30
ElysianProductions <53119667+ElysianProductions@users.noreply.github.com>
2021-09-19 18:25:49 -0400

    Add files via upload

--- UMTK PowerShell GUI/UMTK.ps1
+++ UMTK PowerShell GUI/UMTK.ps1
@@ -3,7 +3,80 @@
 
 Function DomainUser 
 {
-    Write-Host "Domain User"
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+    $user_screens = [System.Windows.Forms.Screen]::AllScreens
+    foreach($screen in $user_screens)
+    {
+        $screen_name = $screen.DeviceName
+        $width = $screen.Bounds.Width
+        $height = $screen.Bounds.Height
+        $primary = $screen.Primary
+    }
+
+    $Domain_Form = New-Object System.Windows.Forms.Form
+    $Domain_Form.Text = "UMTK: Domain User Creation"
+    $Domain_Form.Size = New-Object System.Drawing.Size(600, 600)
+    $Domain_Form.minimumSize = New-Object System.Drawing.Size(600,600) 
+    $Domain_Form.maximumSize = New-Object System.Drawing.Size(600,600) 
+    $Domain_Form.FormBorderStyle = 'Fixed3D'
+
+
+    $employee_name_label = New-Object Windows.Forms.Label
+    $employee_name_label.size = New-Object System.Drawing.Size(150, 50)
+    $employee_name_label.location = New-Object System.Drawing.Size(0, 50)
+    $employee_name_label.text = "Employee name:"
+    $Domain_Form.Controls.Add($employee_name_label)
+
+
+    $employee_name_input = New-Object Windows.Forms.TextBox
+    $employee_name_input.size = New-Object System.Drawing.Size(180, 50)
+    $employee_name_input.location = New-Object System.Drawing.Size(150, 50)
+    $Domain_Form.Controls.Add($employee_name_input)
+
+
+    $username_label = New-Object Windows.Forms.Label
+    $username_label.size = New-Object System.Drawing.Size(150, 50)
+    $username_label.location = New-Object System.Drawing.Size(0, 100)
+    $username_label.text = "Username:"
+    $Domain_Form.Controls.Add($username_label)
+
+
+    $username_input = New-Object Windows.Forms.TextBox
+    $username_input.size = New-Object System.Drawing.Size(50, 50)
+    $username_input.location = New-Object System.Drawing.Size(150, 100)
+    $Domain_Form.Controls.Add($username_input)
+
+
+    $password_label = New-Object Windows.Forms.Label
+    $password_label.size = New-Object System.Drawing.Size(150, 50)
+    $password_label.location = New-Object System.Drawing.Size(0, 150)
+    $password_label.text = "Password:"
+    $Domain_Form.Controls.Add($password_label)
+
+
+    $password_input = New-Object Windows.Forms.TextBox
+    $password_input.size = New-Object System.Drawing.Size(50, 50)
+    $password_input.location = New-Object System.Drawing.Size(200, 150)
+    $Domain_Form.Controls.Add($password_input)
+
+    $email_label = New-Object Windows.Forms.Label
+    $email_label.size = New-Object System.Drawing.Size(150, 50)
+    $email_label.location = New-Object System.Drawing.Size(0, 350)
+    $email_label.text = "Email address:"
+    $Domain_Form.Controls.Add($email_label)
+
+
+    $email_input = New-Object Windows.Forms.TextBox
+    $email_input.size = New-Object System.Drawing.Size(350, 75)
+    $email_input.location = New-Object System.Drawing.Size(0, 350)
+    $Domain_Form.Controls.Add($email_input)
+
+
+
+    $Domain_Form.Add_Shown({$Domain_Form.Activate()})
+    [void] $Domain_Form.ShowDialog()
 }
 
 
@@ -14,7 +87,7 @@ Function LocalUser
 
 Function EditUser 
 {
-
+    Write-Host "Edit User"
 }
 
 Function Main 
@@ -47,11 +120,18 @@ Function Main
 
     $luser_button = New-Object System.Windows.Forms.Button 
     $luser_button.Size = New-Object System.Drawing.Size(120, 30)
-    $luser_button.Location = New-Object System.Drawing.Size(230, 290)
+    $luser_button.Location = New-Object System.Drawing.Size(230, 280)
     $luser_button.Text = "Local User"
     $luser_button.Add_Click({LocalUser})
     $UMTK_Form.Controls.Add($luser_button)
 
+
+    $euser_button = New-Object System.Windows.Forms.Button 
+    $euser_button.Size = New-Object System.Drawing.Size(120, 30)
+    $euser_button.Location = New-Object System.Drawing.Size(230, 310)
+    $euser_button.Text = "Edit User"
+    $euser_button.Add_Click({EditUser})
+    $UMTK_Form.Controls.Add($euser_button)
 
 
     $UMTK_Form.Add_Shown({$UMTK_Form.Activate()})
