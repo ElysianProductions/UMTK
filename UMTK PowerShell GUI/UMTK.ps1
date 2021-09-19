2e48355ee2cbd6b0c09e92e082f5551dc463bc06
ElysianProductions <53119667+ElysianProductions@users.noreply.github.com>
2021-09-19 11:22:52 -0400

    Add files via upload
    This is the beginning of the implementation of the native PS gui.

---
+++ UMTK PowerShell GUI/UMTK.ps1
@@ -0,0 +1,37 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+Function Main 
+{
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+    $user_screens = [System.Windows.Forms.Screen]::AllScreens
+    foreach($screen in $user_screens)
+    {
+        $screen_name = $screen.DeviceName
+        $width = $screen.Bounds.Width
+        $height = $screen.Bounds.Height
+        $primary = $screen.Primary
+    }
+
+    $UMTK_Form = New-Object System.Windows.Forms.Form
+    $UMTK_Form.Text = "User Maintenance Tool Kit"
+    $UMTK_Form.Size = New-Object System.Drawing.Size($width, $height)
+
+
+
+
+    $UMTK_Form.Add_Shown({$UMTK_Form.Activate()})
+    [void] $UMTK_Form.ShowDialog()
+}
+
+Main
+
+# This is straightforward, it will be a lightweight easily modifiable script for Windows only.
+# It will follow the same layout as the C++ GUI does to the letter. The purpose of creating 
+# this is to ensure that those comfortable with PS can use and tailor to their needs. The 
+# overal intent of the C++ program is to prove an easily usable, portable system for the 
+# addition, removal, and or modification of a user account on any system. It will essentially 
+# be the communcation utility which executes the same or very similar commands in the hard coded
+# native scripted utilities. The reason these are going to be GUI based is because people want things 
+# to be as easy as possible while preventing as many mistakes as possible.
