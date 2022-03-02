e92b5a0d6db0d2ef4a346d3bd2a1b13dc8307859
ElysianProductions <53119667+ElysianProductions@users.noreply.github.com>
2022-03-02 13:11:41 -0500

    Add files via upload

--- C++ UMTK/UMTK QML/main.cpp
+++ C++ UMTK/UMTK QML/main.cpp
@@ -11,7 +11,6 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    //PSIntegration psi;
     qmlRegisterType<PSIntegration>("com.elysianproductions.psintegration", 1, 0, "PSIntegration");
     qmlRegisterType<DomainIntegration>("com.elysianproductions.domainintegration", 1, 0, "DomainIntegration");
 
@@ -26,17 +25,6 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
-
-   /* QQmlComponent component(&engine, QUrl::fromLocalFile("LocalAccountWidget.qml"));
-    QObject *la_page = component.create();
-    QObject *la_ca_button = la_page->findChild<QObject* >("la_ca_button");
-
-
-
-
-    QObject::connect(la_ca_button, SIGNAL(requestLocalAccountCreation(string, string, string, bool)), &psi,
-                     SLOT(create_local_account(const QString&,  const QString&, const QString&, const bool&)) );*/
-
 
     return app.exec();
 }
